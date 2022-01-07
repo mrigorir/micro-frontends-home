f31b5c3d2bebc0223e863a00d8a4898d69d787c7
Marco Parra <marcoparra311@gmail.com>
2022-01-07 12:40:33 -0400

    Create a third server callded: server

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "home/Header";
 import Footer from "home/Footer";
+import SafeComponent from '../components/SafeComponent';
 
 import "./index.scss";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
+    <SafeComponent>
+      <Header />
+    </SafeComponent>
     <div className="my-10">
       PDP Page Content
     </div>
